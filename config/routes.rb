bdc881fee6f6b0e7e7a577e93025e10a931f4a8f
Michael Delcambre <delcambrem@gmail.com>
2022-02-16 18:31:02 -0600

    whatever

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
   
-  # resources :reviews
-  resources :appointments, only: [:index, :create, :update, :destroy]
-  resources :doctors, only: [:index]
+  namespace :api do
+    resources :appointments, only: [:index, :create, :update, :destroy]
+    resources :doctors, only: [:index]
+    
+    post '/users', to: "users#create"
+    post '/login', to: "sessions#login"
+    get '/authorized_user', to: "users#show"
+    delete '/logout', to: "sessions#logout"
+  end
   
-  post '/users', to: "users#create"
-  post '/login', to: "sessions#login"
-  get '/authorized_user', to: "users#show"
-  delete '/logout', to: "sessions#logout"
-  
-  # Routing logic: fallback requests for React Router.
-  # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+  
 end
