6ef3792d91ae383e2d70c4761dd71eeb497ffe41
Michael Delcambre <delcambrem@gmail.com>
2022-02-16 21:50:50 -0600

    no message

--- client/src/Components/Home/Home.js
+++ client/src/Components/Home/Home.js
@@ -6,7 +6,7 @@ export default function Home({ user }) {
   
   return (
     <>
-      <h3>Hello {user ? user.first_name : null}! Welcome to Pickadocm</h3>
+      <h3>Hello {user ? user.first_name : null}! Welcome to Pickadoc</h3>
     <div className="div6">Find the doctor who's right for you, when and where you need it. The best doctors in your area are included here.</div>
     </>
   )
